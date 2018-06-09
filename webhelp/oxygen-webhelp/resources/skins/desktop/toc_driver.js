049e144d0a0e46f7dda6b533d65a202ac5d32d5c
Andrew Sales <andrew@andrewsales.com>
2018-06-09 14:50:44 +0100

    [MOD] typo

--- webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180609071343", "oxygen-webhelp/search/index-1.js?uniqueId=20180609071343", "oxygen-webhelp/search/index-2.js?uniqueId=20180609071343", "oxygen-webhelp/search/index-3.js?uniqueId=20180609071343"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-1.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-2.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-3.js?uniqueId=20180609024729"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
