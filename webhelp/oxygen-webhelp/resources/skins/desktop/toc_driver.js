10f379dcc37bd04785358f143278494a493e30fa
Ari Nordström <ari.nordstrom@gmail.com>
2018-06-27 08:51:52 +0200

    New paper version from Rob

--- webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-1.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-2.js?uniqueId=20180609024729", "oxygen-webhelp/search/index-3.js?uniqueId=20180609024729"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20180627084556", "oxygen-webhelp/search/index-1.js?uniqueId=20180627084556", "oxygen-webhelp/search/index-2.js?uniqueId=20180627084556", "oxygen-webhelp/search/index-3.js?uniqueId=20180627084556"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
