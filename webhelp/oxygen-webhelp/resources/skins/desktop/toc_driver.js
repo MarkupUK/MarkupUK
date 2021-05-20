d36fc5717204d4ee7013cec117facfdc668f2645
Andrew Sales <andrew@andrewsales.com>
2021-05-20 23:25:58 +0100

    Re-generated

--- webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20210520103423", "oxygen-webhelp/search/index-1.js?uniqueId=20210520103423", "oxygen-webhelp/search/index-2.js?uniqueId=20210520103423", "oxygen-webhelp/search/index-3.js?uniqueId=20210520103423"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20210520111625", "oxygen-webhelp/search/index-1.js?uniqueId=20210520111625", "oxygen-webhelp/search/index-2.js?uniqueId=20210520111625", "oxygen-webhelp/search/index-3.js?uniqueId=20210520111625"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
