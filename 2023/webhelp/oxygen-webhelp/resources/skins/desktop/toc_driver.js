7ea3445b0f7fa04870d787733c487954f2d00c46
Andrew Sales <andrew@andrewsales.com>
2023-06-17 12:13:32 +0100

    Reinck edit and images.

--- 2023/webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ 2023/webhelp/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20230616110748", "oxygen-webhelp/search/index-1.js?uniqueId=20230616110748", "oxygen-webhelp/search/index-2.js?uniqueId=20230616110748", "oxygen-webhelp/search/index-3.js?uniqueId=20230616110748"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20230617120230", "oxygen-webhelp/search/index-1.js?uniqueId=20230617120230", "oxygen-webhelp/search/index-2.js?uniqueId=20230617120230", "oxygen-webhelp/search/index-3.js?uniqueId=20230617120230"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
